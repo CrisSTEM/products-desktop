e816756907414065335ede4c70a37d541a1e035d
Cristian Garcia <cristianagguniversity@gmail.com>
2021-05-20 19:21:56 -0600

    Creating a Frame Container and Name Imput

--- index.py
+++ index.py
@@ -5,9 +5,18 @@ import sqlite3
 class Product:
     def __init__(self, window):
         self.wind = window
-        self.wind.tittle('Products Application')
+        self.wind.title('Products Application')
+
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Register A new Product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        #Name Input
+        Label(frame, text='Name: ').grid(row=1,column=0)
+        self.name = Entry(frame)
+        self.name.grid(row=1,column=1)
 
 if __name__ == '__main__':
-    window = TK()   
+    window = Tk()   
     application = Product(window)
     window.mainloop()
