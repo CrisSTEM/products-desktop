93c3b811326ca0ec9ba8706c163024d794676d1d
Cristian Garcia <cristianagguniversity@gmail.com>
2021-05-20 19:25:27 -0600

    Add Price Input and Buttom Add Product

--- index.py
+++ index.py
@@ -11,10 +11,18 @@ class Product:
         frame = LabelFrame(self.wind, text = 'Register A new Product')
         frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
 
-        #Name Input
+        # Name Input
         Label(frame, text='Name: ').grid(row=1,column=0)
         self.name = Entry(frame)
         self.name.grid(row=1,column=1)
+
+        # Price Input
+        Label(frame, text='Price: ').grid(row=2, column=0)
+        self.price = Entry(frame)
+        self.price.grid(row=2,column=1)
+
+        # Buttom Add Product
+        ttk.Button(frame, text='Save product').grid(row=3,columnspan=2,sticky= W + E)
 
 if __name__ == '__main__':
     window = Tk()   
